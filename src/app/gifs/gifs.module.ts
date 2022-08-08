66e0a716a365b122ded49b3ff5232de2b87ebee5
Vittorio Adesso <adessovittorio8@gmail.com>
2022-08-08 13:04:19 +0200

    adding new components & struture

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GifsPagesComponent } from './gifs-pages/gifs-pages.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { ResultsOFsearchComponent } from './results-ofsearch/results-ofsearch.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    GifsPagesComponent,
+    SearchbarComponent,
+    ResultsOFsearchComponent
+  ],
+  exports:[
+    GifsPagesComponent,
+  ],
   imports: [
     CommonModule
   ]
---
+++ src/app/gifs/results-ofsearch/results-ofsearch.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-results-ofsearch',
+  templateUrl: './results-ofsearch.component.html',
+  styles: [
+  ]
+})
+export class ResultsOFsearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
