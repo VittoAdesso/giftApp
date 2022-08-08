abbb5afb8b0a5d1cf548252e2d602663538e2787
Vittorio Adesso <adessovittorio8@gmail.com>
2022-08-08 19:53:59 +0200

    adding localstorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -14,7 +14,9 @@ export class GifsService {
     return[...this._historial]
   }
   
-  constructor( private http: HttpClient){}
+  constructor( private http: HttpClient){
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+  }
 
   gifSearch( query: string = ''){
     query = query.trim().toLowerCase();
@@ -22,6 +24,9 @@ export class GifsService {
     if( !this._historial.includes( query )) {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0,10); // cut betwen 0 and 10
+
+      // host & save
+      localStorage.setItem('historial', JSON.stringify(this._historial));
     }
     this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=5ZYuirHeyfsl4Qd8gCmSjwqRyfJUmX44&q=${query}&limit=10`)
       .subscribe((response ) => {
