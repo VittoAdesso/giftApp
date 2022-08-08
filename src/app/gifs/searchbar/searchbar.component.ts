1c3dd76b4cf32183ab04533759d9b0cc87288c0b
Vittorio Adesso <adessovittorio8@gmail.com>
2022-08-08 13:22:47 +0200

    use viewChild

--- src/app/gifs/searchbar/searchbar.component.ts
+++ src/app/gifs/searchbar/searchbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-searchbar',
@@ -11,6 +11,14 @@ export class SearchbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @ViewChild('txtSearch') txtSearch!: ElementRef;
+
+  search( ): void {
+    const valor = this.txtSearch.nativeElement.value;
+    console.log(valor);
+    this.txtSearch.nativeElement.value = ''; // to erase the input 
   }
 
 }
